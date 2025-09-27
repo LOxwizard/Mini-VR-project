4817930cd8550f55a8afbdd6a41a44273fce301c
LOxwizard <bhemanth213@gmail.com>
2025-09-27 19:00:47 +0530

    Initial

---
+++ Assets/player.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour
+{
+    public Transform aimTarget;
+    float speed = 3f;
+    bool hitting;
+    float force = 13;
+    Animator animator;
+    public Transform ball;
+    Vector3 aimTargetInitialPosition;
+
+   
+    private void Start()
+    {
+        animator = GetComponent<Animator>();    
+        aimTargetInitialPosition = aimTarget.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            hitting = true;
+        }else if (Input.GetKeyUp(KeyCode.F))
+        {
+            hitting = false;
+        }
+
+        if (hitting)
+        {
+            aimTarget.Translate(new Vector3(h, 0, 0) * speed*2 * Time.deltaTime);
+        }
+
+        if ((h != 0 || v != 0) && !hitting)
+        {
+            transform.Translate(new Vector3(h, 0, v) * speed * Time.deltaTime);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Ball"))
+        {
+            Vector3 dir = aimTarget.position - transform.position ;
+            other.GetComponent<Rigidbody>().velocity = dir.normalized * force + new Vector3(0,5,0);
+
+            Vector3 ballDir = ball.position - transform.position;
+            if (ballDir.x > 0)
+            {
+                animator.Play("forehand");
+            }
+            else
+            {
+                animator.Play("backhand");
+            }
+            aimTarget.position = aimTargetInitialPosition;
+        }
+    }
+}
